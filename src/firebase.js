9050ee6847aa595a1ad1ab838539bafb15deba2d
Ritwik783 <ritwikhalder783@outlook.com>
2020-12-21 22:11:46 +0530

    before search button

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8R8Yu_dBr9W8Q3Bpk0PFR5W3D7VQ9DWU",
+    authDomain: "whatasapp-clone.firebaseapp.com",
+    projectId: "whatasapp-clone",
+    storageBucket: "whatasapp-clone.appspot.com",
+    messagingSenderId: "707011583637",
+    appId: "1:707011583637:web:c3c191392cecdfbd57459c",
+    measurementId: "G-FYLMX0SKNB"
+  };
