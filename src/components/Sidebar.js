7d42e9494a84df9b584e55482da9d807c2748f34
Ritwik783 <ritwikhalder783@outlook.com>
2020-12-22 13:05:17 +0530

    searchbar done 90%

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 
 function Sidebar() {
     return (
@@ -12,7 +13,7 @@ function Sidebar() {
                 <IconButton>
                     <Avatar />
                 </IconButton>
-                
+
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcon />
@@ -25,7 +26,12 @@ function Sidebar() {
                     </IconButton>
                 </div>
             </div>
-            <div className="sidebar__search"></div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search or start a new chat.."/>
+                </div>
+            </div>
             <div className="sidebar__chats"></div>
         </div>
     )
